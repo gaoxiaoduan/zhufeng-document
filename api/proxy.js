ed0f6ffd8b0f47c70b7617dcb483fe8840acda12
xiaoduan.KD <1961018000@qq.com>
2022-03-18 14:22:27 +0800

    chore:添加start命令

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,6 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
   let target;
-  console.log("req.url,,,",req.url);
   if (req.url.startsWith("/imgApi")) {
     // 将包含 的接口转发到 http://img.zhufengpeixun.cn请求
     target = "http://img.zhufengpeixun.cn";
@@ -11,7 +10,7 @@ module.exports = (req, res) => {
     target,
     changeOrigin: true,
     pathRewrite: {
-      // http://img.zhufengpeixun.cn/api/syncfunc.png
+      // http://img.zhufengpeixun.cn/imgApi/syncfunc.png
       // ⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
       // http://img.zhufengpeixun.cn/syncfunc.png
       "^/imgApi": "/",
